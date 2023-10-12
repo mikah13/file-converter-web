d4eb4ce5cbf31ab6973c3564b6460464377733e8
mikah13 <anhminhhoang13@gmail.com>
2023-10-11 23:50:47 -0700

    deploy

--- src/components/statistics.tsx
+++ src/components/statistics.tsx
@@ -3,7 +3,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
-import prisma from "@/lib/prisma";
 import { formatBytes } from "@/lib/utils";
 
 async function getMetaData() {
@@ -17,8 +16,6 @@ function Statistics() {
     queryFn: () => getMetaData(),
     refetchInterval: 1000,
   });
-  // const { totalSize, fileCounts } = data;
-  console.log(data);
   return (
     <div className="absolute bottom-0 left-0 flex h-10 w-96 flex-row justify-evenly border-r  border-t text-sm">
       <div className="flex h-full w-full flex-row items-center justify-evenly border-r px-2">
